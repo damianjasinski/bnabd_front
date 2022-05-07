1e1482e67bb0f304945de3e3f1117bc08d400496
Damian <pk-14@sr.dmcs.pl>
2022-05-07 18:43:09 +0200

    Changes login screen background

--- src/Guest/Register.js
+++ src/Guest/Register.js
@@ -8,8 +8,7 @@ import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Navbar from "./Navbar";
-import Footer from "./Footer";
-import logo from "../Resource/cinema-09.jpg"; // with import
+import logo from "../Resource/cinema-09.jpg";
 
 const theme = createTheme({
   palette: {
@@ -23,6 +22,7 @@ const backgroundStyle = {
     borderRadius: "0",
   },
 };
+
 const StyledButton = styled(Button)`
   background-color: black;
   color: #e87800;
@@ -30,7 +30,7 @@ const StyledButton = styled(Button)`
   font-weight: bold;
   padding: 8px 16px;
   &:hover {
-    background-color: #2F2F31;
+    background-color: #2f2f31;
   }
   &:focus {
     background-color: gray;
@@ -94,91 +94,99 @@ export default function Register() {
   return (
     <ThemeProvider theme={theme}>
       <Navbar />
-      <Paper style={backgroundStyle.paperContainer} sx = {{width:"auto"}}>
+      <Paper
+        style={backgroundStyle.paperContainer}
+        sx={{ backgroundSize: "auto" }}
+      >
         <Box sx={{ mt: 2 }}>
           <Grid container spacing={2}>
-            <Grid item lg={8} sx={{}}>
+            <Grid item lg={7} sx={{}}>
               <Box
                 sx={{
                   color: "text.secondary",
-
-                  m: 3,
                 }}
               ></Box>
             </Grid>
 
             <Grid
               spacing={0}
-              style={{ minHeight: "72vh" }}
+              style={{ minHeight: "100vh" }}
               item
-              lg={4}
-              sx={{ mt: 30 }}
+              lg={5}
+              sx={{ bgcolor: "white" }}
+              container
+              
             >
               <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
                 sx={{
                   color: "text.secondary",
-                  m: 3,
+                  mx: "auto"
                 }}
               >
                 <form noValidate onSubmit={handleSubmit}>
-                  <Box sx={{ my: 3 }}>
-                    <TextField
-                      onChange={(e) => setfirstName(e.target.value)}
-                      label="Imię"
-                      variant="outlined"
-                      color="secondary"
-                      fullWidth
-                      required
-                      InputProps={{ style: { fontSize: 20 } }}
-                      InputLabelProps={{ style: { fontSize: 20 } }}
-                      error={firstNameError}
-                      sx={{ width: "30vh" }}
-                    />
-                  </Box>
-                  <Box sx={{ my: 3 }}>
-                    <TextField
-                      sx={{ width: "30vh" }}
-                      onChange={(e) => setEmail(e.target.value)}
-                      label="e-mail"
-                      variant="outlined"
-                      color="secondary"
-                      fullWidth
-                      required
-                      InputProps={{ style: { fontSize: 20 } }}
-                      InputLabelProps={{ style: { fontSize: 20 } }}
-                      error={emailError}
-                    />
-                  </Box>
+                  <Box sx={{ mx: 5, p: 10 }}>
+                    <Box sx={{ my: 3 }}>
+                      <TextField
+                        onChange={(e) => setfirstName(e.target.value)}
+                        label="Imię"
+                        variant="outlined"
+                        color="secondary"
+                        fullWidth
+                        required
+                        InputProps={{ style: { fontSize: 20 } }}
+                        InputLabelProps={{ style: { fontSize: 20 } }}
+                        error={firstNameError}
+                        sx={{ width: "30vh" }}
+                      />
+                    </Box>
+                    <Box sx={{ my: 3 }}>
+                      <TextField
+                        sx={{ width: "30vh" }}
+                        onChange={(e) => setEmail(e.target.value)}
+                        label="e-mail"
+                        variant="outlined"
+                        color="secondary"
+                        fullWidth
+                        required
+                        InputProps={{ style: { fontSize: 20 } }}
+                        InputLabelProps={{ style: { fontSize: 20 } }}
+                        error={emailError}
+                      />
+                    </Box>
 
-                  <Box sx={{ my: 3 }}>
-                    <TextField
-                      sx={{ width: "30vh"}}
-                      onChange={(e) => setPassword(e.target.value)}
-                      label="Hasło"
-                      variant="outlined"
-                      color="secondary"
-                      fullWidth
-                      required
-                      InputProps={{ style: { fontSize: 20 } }}
-                      InputLabelProps={{ style: { fontSize: 20 } }}
-                      error={passwordError}
-                    />
+                    <Box sx={{ my: 3 }}>
+                      <TextField
+                        sx={{ width: "30vh" }}
+                        onChange={(e) => setPassword(e.target.value)}
+                        label="Hasło"
+                        variant="outlined"
+                        color="secondary"
+                        fullWidth
+                        required
+                        InputProps={{ style: { fontSize: 20 } }}
+                        InputLabelProps={{ style: { fontSize: 20 } }}
+                        error={passwordError}
+                      />
+                    </Box>
+                    <Box sx={{ my: 3 }}>
+                      <TextField
+                        sx={{ width: "30vh" }}
+                        onChange={(e) => setRetryPassword(e.target.value)}
+                        label="Powtórz hasło"
+                        variant="outlined"
+                        color="secondary"
+                        fullWidth
+                        InputProps={{ style: { fontSize: 22 } }}
+                        InputLabelProps={{ style: { fontSize: 22 } }}
+                        required
+                        error={retryPasswordError}
+                      />
+                    </Box>
+                    <StyledButton>Submit</StyledButton>
                   </Box>
-                  <Box sx={{ my: 3 }}>
-                    <TextField
-                      sx={{ width: "30vh" }}
-                      onChange={(e) => setRetryPassword(e.target.value)}
-                      label="Powtórz hasło"
-                      variant="outlined"
-                      color="secondary"
-                      fullWidth
-                      InputProps={{ style: { fontSize: 22 } }}
-                      InputLabelProps={{ style: { fontSize: 22 } }}
-                      required
-                      error={retryPasswordError}
-                    />
-                  </Box>
-                  <StyledButton>Submit</StyledButton>
                 </form>
               </Box>
             </Grid>
