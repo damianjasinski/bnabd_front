7124f1b7e986c34f7ac59da458fbedbfe02a8d31
damian <damian.kania94@gmail.com>
2022-05-07 15:51:19 +0200

    add forms

--- src/Guest/Register.js
+++ src/Guest/Register.js
@@ -1,9 +1,164 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TextField } from '@mui/material'
+import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
+import LoginIcon from '@mui/icons-material/Login';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function Register() {
+  const [firstName, setfirstName] = useState('')
+  const [firstNameError, setfirstNameError] = useState(false)
+
+  const [email, setEmail] = useState('')
+  const [emailError, setEmailError] = useState(false)
+
+  const [password, setPassword] = useState('')
+  const [passwordError, setPasswordError] = useState(false)
+
+  const [retryPassword, setRetryPassword] = useState('')
+  const [retryPasswordError, setRetryPasswordError] = useState(false)
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault()
+
+    if (firstName === '') {
+      setfirstNameError(true)
+    } else {
+      setfirstNameError(false)
+    }
+
+    if (email === '') {
+      setEmailError(true)
+    } else {
+      setEmailError(false)
+    }
+
+    if (password === '') {
+      setPasswordError(true)
+    } else {
+      setPasswordError(false)
+    }
+
+    if (retryPassword === '') {
+      setRetryPasswordError(true)
+    } else {
+      setRetryPasswordError(false)
+    }
+
+    console.log(firstName)
+    console.log(email)
+    console.log(password)
+    console.log(retryPassword)
+
+  }
   return (
-    <div>
-      Register
-    </div>
+    <Box sx={{
+      height: '100%',
+      backgroundColor: 'black'
+    }}>
+
+    <Grid container spacing={2}>
+
+
+      <Grid item lg={7}>
+      <Box sx={{
+        color: 'text.secondary',
+        
+        m: 3
+      }}>
+        <Item>xs=8</Item>
+
+      </Box>
+      </Grid>
+
+      <Grid item lg={5}>
+
+      
+      <Box sx={{
+        color: 'text.secondary',
+        m: 3
+      }}>
+
+        <form noValidate onSubmit={handleSubmit}>
+
+          <Box sx={{ my: 3 }}>
+            <TextField
+              onChange={(e) => setfirstName(e.target.value)}
+              label="Imię"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              required
+              error={firstNameError}
+            />
+          </Box>
+          <Box sx={{ my: 3 }}>
+            <TextField
+              onChange={(e) => setEmail(e.target.value)}
+              label="e-mail"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              required
+              error={emailError}
+            />
+          </Box>
+
+          <Box sx={{ my: 3 }}>
+            <TextField
+              onChange={(e) => setPassword(e.target.value)}
+              label="Hasło"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              required
+              error={passwordError}
+            />
+          </Box>
+          <Box sx={{ my: 3 }}>
+            <TextField
+              onChange={(e) => setRetryPassword(e.target.value)}
+              label="Powtórz hasło"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              required
+              error={retryPasswordError}
+            />
+          </Box>
+
+          <Button
+            type="submit"
+            color="secondary"
+            variant="contained"
+            endIcon={<LoginIcon />}
+          >
+            Submit
+          </Button>
+
+        </form>
+
+
+      </Box>
+
+
+      </Grid>
+    </Grid>
+
+</Box>
+
   )
 }
