f056e2e25dc274de8d25ef17daf8d9dc55acc8e7
damian <damian.kania94@gmail.com>
2022-05-07 19:04:38 +0200

    Seat init

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import { ButtonGroup } from '@mui/material'
+import React, { useState } from 'react'
+import IconButton from '@mui/material/IconButton';
+import WeekendIcon from '@mui/icons-material/Weekend';
+import { Box } from '@mui/system';
+
+const iconSize = "large"
+
+
 
 export default function Orders() {
+  var arr = Array(100).fill("error");
+
+  const [seat, setSeatState] = useState(arr);
+  const handleSubmit = (event) => {
+    const id = event.target.name;
+  
+  }
+
   return (
-    <div>
-      Orders
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        
+      }}
+    >
+
+
+<ButtonGroup variant="contained">
+      <IconButton  name='1' color={seat[0]} size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color={seat[1]} size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+
+      </ButtonGroup>
+
+      <ButtonGroup variant="contained">
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      </ButtonGroup>
+
+      <ButtonGroup variant="contained">
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      <IconButton color="warning" size="large"> <WeekendIcon fontSize={iconSize}/> </IconButton>
+      </ButtonGroup>
+
+      
+
+
+    </Box>
+      
+    
   )
 }
