d2dda96bfa2c68513872bbc75f24a57c76dec607
Damian <pk-14@sr.dmcs.pl>
2022-05-08 15:58:16 +0200

    Add login requests

--- src/Guest/Login.js
+++ src/Guest/Login.js
@@ -1,9 +1,177 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { TextField } from "@mui/material";
+import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
+import LoginIcon from "@mui/icons-material/Login";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Navbar from "./Navbar";
+import logo from "../Resource/cinema-09.jpg";
+const axios = require("axios").default;
 
-export default function Login() {
+const theme = createTheme({
+  palette: {
+    type: "darkTheme",
+  },
+});
+
+const backgroundStyle = {
+  paperContainer: {
+    backgroundImage: `url(${logo})`,
+    borderRadius: "0",
+  },
+};
+
+const StyledButton = styled(Button)`
+  background-color: black;
+  color: #e87800;
+  font-size: 18px;
+  font-weight: bold;
+  padding: 8px 16px;
+  &:hover {
+    background-color: #2f2f31;
+  }
+  &:focus {
+    background-color: gray;
+  }
+`;
+
+export default function Register() {
+
+  const [response, setResponse] = useState([]);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [inputs, setInputs] = useState({"email":"","password":""});
+
+
+  const loginRequest = () => {
+      const user = {email : inputs.email, password : inputs.password}
+      const data = axios
+        .post("http://localhost:8080/api/login/signin", user)
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      setResponse(data.data);
+  }
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+    console.log(inputs)
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (inputs.email === "") {
+      setEmailError(true);
+    } 
+    else {
+      setEmailError(false);
+    }
+    if (inputs.password === "") {
+      setPasswordError(true);
+    } 
+    else {
+      setPasswordError(false);
+    }
+    if (emailError == false && passwordError == false) {
+      loginRequest();
+    }
+  };
+
   return (
-    <div>
-      Login
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Paper
+        style={backgroundStyle.paperContainer}
+        sx={{ backgroundSize: "auto" }}
+      >
+        <Box sx={{ mt: 2 }}>
+          <Grid container spacing={2}>
+            <Grid item lg={7} sx={{}}>
+              <Box
+                sx={{
+                  color: "text.secondary",
+                }}
+              ></Box>
+            </Grid>
+
+            <Grid
+              spacing={0}
+              style={{ minHeight: "100vh" }}
+              item
+              lg={5}
+              sx={{ bgcolor: "white" }}
+              container
+            >
+              <Box
+                sx={{
+                  color: "text.secondary",
+                  mx: "auto",
+                  mt: "auto",
+                }}
+              >
+                <Typography variant="h1" component="h2">
+                  Zaloguj się
+                </Typography>
+              </Box>
+
+              <Box
+                sx={{
+                  color: "text.secondary",
+                  mx: "auto",
+                }}
+              >
+                <form noValidate onSubmit={handleSubmit}>
+                  <Box sx={{ mx: 5, p: 10 }}>
+                    <Box sx={{ my: 3 }}>
+                      <TextField
+                        sx={{ width: "40vh" }}
+                        onChange={handleChange}
+                        label="e-mail"
+                        variant="outlined"
+                        name = "email"
+                        color="secondary"
+                        fullWidth
+                        required
+                        InputProps={{ style: { fontSize: 20 } }}
+                        InputLabelProps={{ style: { fontSize: 20 } }}
+                        error={emailError}
+                      />
+                    </Box>
+
+                    <Box sx={{ my: 3 }}>
+                      <TextField
+                        sx={{ width: "40vh" }}
+                        onChange={handleChange}
+                        label="Hasło"
+                        type = "password"
+                        variant="outlined"
+                        name = "password"
+                        color="secondary"
+                        fullWidth
+                        required
+                        InputProps={{ style: { fontSize: 20 } }}
+                        InputLabelProps={{ style: { fontSize: 20 } }}
+                        error={passwordError}
+                      />
+                    </Box>
+                    <StyledButton onClick = {handleSubmit}>Submit</StyledButton>
+                  </Box>
+                </form>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+      </Paper>
+    </ThemeProvider>
+  );
 }
