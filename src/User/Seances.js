4ff3d4559acefd235baba233fa340c039c18303d
Damian <pk-14@sr.dmcs.pl>
2022-05-11 18:39:19 +0200

    Add react pure carousel

--- src/User/Seances.js
+++ src/User/Seances.js
@@ -1,51 +1,45 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { Navigate } from "react-router-dom";
-import Container from '@mui/material/Container';
+import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 const backgroundStyle = {
   paperContainer: {
     backgroundColor: `black`,
     borderRadius: "0",
-    minHeight: "100vh"
+    minHeight: "100vh",
   },
 };
 
-const card = (
-  <React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Picu Bicu
-      </Typography>
-      <Typography variant="h5" component="div">
-        grasz
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        w
-      </Typography>
-      <Typography variant="body2">
-        FORTNIT?
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
-  </React.Fragment>
-);
+const carouselButtonStyles = {
+  button: {
+    borderStyle: "solid",
+    borderWidth: "1.5px",
+    borderRadius: "5px",
+    color: "#e87800",
+    fontSize: "18px",
+    fontWeight: "bold",
+    padding: "8px 16px",
+  },
+};
 
 export default function Seances() {
-
-
   if (!sessionStorage.getItem("user")) {
     return <Navigate to="/redirect" />;
   }
@@ -57,42 +51,33 @@ export default function Seances() {
     >
       <div sx={{ backgroundColor: "black" }}>
         <Navbar />
-        <Container >
-
+        <Container>
           <Grid container spacing={2} sx={{ mt: "auto" }}>
-            <Grid item xs={4}>
+            <Grid item xs={12}>
               <Box sx={{ minWidth: 50 }}>
-                <Card variant="outlined">{card}</Card>
+                <CarouselProvider
+                  naturalSlideWidth={100}
+                  naturalSlideHeight={15}
+                  visibleSlides={2}
+                  totalSlides={3}
+                >
+                  <Slider>
+                    <Slide index={0}>I am the first Slide.</Slide>
+                    <Slide index={1}>I am the second Slide.</Slide>
+                    <Slide index={2}>I am the third Slide.</Slide>
+                  </Slider>
+                  <Box sx={{ textAlign: "center" }}>
+                    <ButtonBack style={carouselButtonStyles.button}>
+                      Back
+                    </ButtonBack>
+                    <ButtonNext style={carouselButtonStyles.button}>
+                      Next
+                    </ButtonNext>
+                  </Box>
+                </CarouselProvider>
               </Box>
             </Grid>
-            <Grid item xs={4}>
-              <Box sx={{ minWidth: 50 }}>
-                <Card variant="outlined">{card}</Card>
-              </Box>
-            </Grid>
-            <Grid item xs={4}>
-              <Box sx={{ minWidth: 50 }}>
-                <Card variant="outlined">{card}</Card>
-              </Box>
-            </Grid>
-            <Grid item xs={4}>
-              <Box sx={{ minWidth: 50 }}>
-                <Card variant="outlined">{card}</Card>
-              </Box>
-            </Grid>
-            <Grid item xs={4}>
-              <Box sx={{ minWidth: 50 }}>
-                <Card variant="outlined">{card}</Card>
-              </Box>
-            </Grid>
-            <Grid item xs={4}>
-              <Box sx={{ minWidth: 50 }}>
-                <Card variant="outlined">{card}</Card>
-              </Box>
-            </Grid>
-
           </Grid>
-
         </Container>
       </div>
     </Paper>
