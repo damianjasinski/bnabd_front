8c6f1edaecee5b673bbc09a761639296bb695747
Damian <pk-14@sr.dmcs.pl>
2022-05-22 14:55:09 +0200

    Add better carousel

--- src/User/CardItem.js
+++ src/User/CardItem.js
@@ -1,54 +1,57 @@
-
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 
 const CardItem = (props) => {
-    return (
-        <Box sx={{ mx: "auto" }}>
+  return (
+    <Card sx={{ mx: 1}}>
+      <Grid container spacing={2}>
+        <Grid
+          item
+          xs={7}
+          md={5}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" >
+              {props.seance.titles.name}
+            </Typography>
+            <Typography variant="body1" color="text.secondary" sx ={{textAlign : "left", mt : 2}}>
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+            <Typography variant="h5" color="text.secondary" sx ={{mt : 6,  textAlign : "left"}}>
+              Kiedy : {props.seance.seanceDate}
+            </Typography>
+          </CardContent>
+          <Box sx={{ mt: "auto" , mx :"auto", mb : 1}}>
+            <CardActions>
+              <Button  size="large" sx = {{color: "#e87800", fontWeight: 600}}>Zarezerwuj</Button>
+            </CardActions>
+          </Box>
+        </Grid>
+        <Grid item xs={5} md={7}>
+          <CardMedia
+            component="img"
+            height = "550"
+            image={props.seance.titles.imUrl}
+            alt="green iguana"
+          />
+        </Grid>
+      </Grid>
+    </Card>
+  );
+};
 
-            <Card sx={{ maxWidth: 520, mx: "auto" }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={6} md={5}>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                Lizard
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000
-                                species, ranging across all continents except Antarctica
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small">Share</Button>
-                            <Button size="small">Learn More</Button>
-                        </CardActions>
-                    </Grid>
-                    <Grid item xs={6} md={7}>
-                        <CardMedia
-                            component="img"
-                            height="500"
-                            image={props.imUrl}
-                            alt="green iguana"
-                        />
-                    </Grid>
-                </Grid>
-            </Card>
 
-        </Box >
-    );
-}
-
-CardItem.defaultProps = {
-    name: "",
-    image: ""
-}
-
-export default CardItem
+export default CardItem;
--- src/User/Seances.js
+++ src/User/Seances.js
@@ -3,21 +3,14 @@ import Navbar from "./Navbar";
 import { Navigate } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-
-import CardItem from "./CardItem"
+import Typography from "@mui/material/Typography";
+import CardItem from "./CardItem";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
-import {
-  CarouselProvider,
-  Slider,
-  Slide,
-  ButtonBack,
-  ButtonNext,
-} from "pure-react-carousel";
-import "pure-react-carousel/dist/react-carousel.es.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
 const axios = require("axios").default;
-
-
 
 const backgroundStyle = {
   paperContainer: {
@@ -39,31 +32,27 @@ const carouselButtonStyles = {
   },
 };
 
-
-
 const Seances = () => {
-
-  const [seances, setSeances] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [seances, setSeances] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const token = sessionStorage.getItem("jwt")
+    const token = sessionStorage.getItem("jwt");
     const data = axios
       .get("http://localhost:8080/api/seance/get/current", {
         headers: {
-          'Authorization': `Basic ${token}`
-        }
+          Authorization: `Basic ${token}`,
+        },
       })
       .then(function (response) {
-        console.log(response.data)
-        setSeances(response.data.seances)
-        setLoading(false)
+        setSeances(response.data.seances);
+        setLoading(false);
       })
       .catch(function (error) {
         console.log(error);
       });
-  });
-
+  }, []);
+  console.log(seances);
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -71,41 +60,43 @@ const Seances = () => {
   if (!sessionStorage.getItem("jwt")) {
     return <Navigate to="/redirect" />;
   }
-  
+
   return (
     <Paper
       style={backgroundStyle.paperContainer}
-      sx={{ backgroundSize: "auto" }}
+      sx={{ backgroundSize: "auto"}}
     >
-      
-      <div sx={{ backgroundColor: "black" }}>
+      <Box sx={{ backgroundColor: "black", height: "105vh" }}>
         <Navbar />
         <Container>
-          <Box sx={{ mt: 5}}>
-            <CarouselProvider
-              naturalSlideWidth={100}
-              naturalSlideHeight={90}
-              visibleSlides={2}
-              totalSlides={seances.length}
-            >
-              <Slider>
-                <Slide index={0}><CardItem imUrl = {seances[0].titles.imUrl}></CardItem></Slide>
-                <Slide index={1}><CardItem imUrl = {seances[0].titles.imUrl}></CardItem></Slide>
-              </Slider>
-              <Box sx={{ textAlign: "center" }}>
-                <ButtonBack style={carouselButtonStyles.button}>
-                  Back
-                </ButtonBack>
-                <ButtonNext style={carouselButtonStyles.button}>
-                  Next
-                </ButtonNext>
-              </Box>
-            </CarouselProvider>
+          <Box sx={{ backgroundColor: "#303131", textAlign: "center", height : "4vh"}}>
+            <Typography variant="h5" gutterBottom component="div" sx ={{color: "#e87800"}}>
+              Aktualne seanse
+            </Typography>
           </Box>
         </Container>
-      </div>
+        <Container>
+          <Box sx={{ mt: 6 }}>
+            <Carousel
+              dynamicHeight={true}
+              centerMode={true}
+              swipeable={true}
+              emulateTouch={true}
+              centerSlidePercentage={80}
+            >
+              {seances.map((seance) => {
+                return (
+                  <CardItem
+                    seance = {seance}
+                  ></CardItem>
+                );
+              })}
+            </Carousel>
+          </Box>
+        </Container>
+      </Box>
     </Paper>
   );
-}
+};
 
-export default Seances
+export default Seances;
